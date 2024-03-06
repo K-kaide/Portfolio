886943770f30bdcd377d8353b9217d128a8f3c17
Kaide <paguielt@gmail.com>
2024-03-06 17:11:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const typingTexts = [
+  "Hi there!",
+  "The Name's Jem💎",
+  "Welcome to My Portfolio...",
+]; // Define the texts to be typed
+const typingDelay = 100; // Delay between typing each character
+const newTextDelay = 2000; // Delay before changing to the next text
+
+let textIndex = 0;
+let charIndex = 0;
+
+const typingHeader = document.getElementById("typing-header");
+
+function type() {
+  if (charIndex < typingTexts[textIndex].length) {
+    typingHeader.textContent += typingTexts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingHeader.textContent = typingTexts[textIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, typingDelay);
+  } else {
+    textIndex++;
+    if (textIndex >= typingTexts.length) {
+      textIndex = 0;
+    }
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, typingDelay);
+});
